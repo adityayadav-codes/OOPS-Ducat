2b267691126414038f490f619cded920b1e871ba
adityayadav-codes <adityayadav2905@gmail.com>
2025-12-28 12:28:00 +0530

    data shadowing reference object

--- Tcs2.java
+++ Tcs2.java
@@ -1,14 +1,51 @@
-public class Tcs2{
-    int id;
+// public class Tcs2{
+//     int id;
+//     String name;
+//     int salary;
+//     static String cname = "Tcd";
+
+
+//     void store(int id, String name, int salary) {
+//         this.id = id;
+//         this.name = name;
+//         this.salary = salary;
+//     }
+//     void show () {
+//         System.out.println("Employee Details—————");
+//         System.out.println("Id : " + id);
+//         System.out.println("Name : " + name);
+//         System.out.println("Salary : " + salary);
+//         System.out.println("Company Name : " + cname);
+//         System.out.println("==========================");
+//     }
+//     public static void main (String args []) {
+//         Tcs2 emp1 = new Tcs2();
+//         emp1.store(101, "Aditya Yadav", 28823);
+//         emp1.show();
+//         Tcs2 emp2 = new Tcs2();
+//         emp2.store(102, "Akash Yadav", 32322);
+//         emp2.show();
+//         Tcs2 emp3 = new Tcs2();
+//         emp3.store(103, "Anil Yadav", 32322);           
+//         emp3.show();
+//     }
+// }
+
+
+/*
+    Data Shoding------
+*/
+public class Tcs2 {
+    int id ;
     String name;
     int salary;
-    static String cname = "Tcd";
+    static String cname = "Tcs";
+    void store (int id, String name, int salary,Tcs2 emp)  {
 
-
-    void store(int id, String name, int salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+        emp.id = id;
+        emp.name = name;
+        emp.salary = salary;
+        
     }
     void show () {
         System.out.println("Employee Details—————");
@@ -18,15 +55,15 @@ public class Tcs2{
         System.out.println("Company Name : " + cname);
         System.out.println("==========================");
     }
-    public static void main (String args []) {
+    public static void main(String args []) {
         Tcs2 emp1 = new Tcs2();
-        emp1.store(101, "Aditya Yadav", 28823);
+        emp1.store(101, "Aditya Yadav", 28823,emp1);
         emp1.show();
         Tcs2 emp2 = new Tcs2();
-        emp2.store(102, "Akash Yadav", 32322);
+        emp2.store(102, "Akash Yadav", 32322,emp2);
         emp2.show();
         Tcs2 emp3 = new Tcs2();
-        emp3.store(103, "Anil Yadav", 32322);           
+        emp3.store(103, "Anil Yadav", 32322,emp3);           
         emp3.show();
     }
 }
